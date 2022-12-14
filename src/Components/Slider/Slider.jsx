02900b9acb2aa8404b4eb7eb4c277a45c409432a
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-14 22:52:01 +0530

    add

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import img1 from "../../Assets/Images/image1.jpg"
 import img2 from "../../Assets/Images/image2.jpg"
 import img3 from "../../Assets/Images/image3.jpg"
@@ -8,37 +8,35 @@ import img4 from "../../Assets/Images/image4.jpg"
 import img5 from "../../Assets/Images/image5.jpg"
 import img6 from "../../Assets/Images/image6.jpg"
 import img7 from "../../Assets/Images/image7.jpg"
-
+import "./Slider.css"
 const Slider = () => {
   return (
-        <Carousel>
+        <Carousel className="main-slide">
                 <div>
-                    <img src={img1} alt="pp"/>
-                    <p className="legend">Legend 1</p>
+                    <img src={img1} alt="pp" height="400px" />
                 </div>
                 <div>
-                    <img src={img2}  alt="pp"/>
-                    <p className="legend">Legend 2</p>
+                    <img src={img2}  alt="pp" height="400px" />
                 </div>
                 <div>
-                    <img src={img3} alt="pp" />
-                    <p className="legend">Legend 3</p>
+                    <img src={img3} alt="pp" height="400px"/>
+                   
                 </div>
                 <div>
-                    <img src={img4} alt="pp"/>
-                    <p className="legend">Legend 1</p>
+                    <img src={img4} alt="pp" height="400px" />
+                   
                 </div>
                 <div>
-                    <img src={img5} alt="pp"/>
-                    <p className="legend">Legend 2</p>
+                    <img src={img5} alt="pp" height="400px"/>
+                  
                 </div>
                 <div>
-                    <img src={img6} alt="pp" />
-                    <p className="legend">Legend 3</p>
+                    <img src={img6} alt="pp" height="400px"  />
+                   
                 </div>
                 <div>
-                    <img src={img7}  alt="pp" />
-                    <p className="legend">Legend 3</p>
+                    <img src={img7}  alt="pp" height="400px" />
+                   
                 </div>
             </Carousel>
   )
