9c1c886b230a650e1c3fa5651b8b3001746268aa
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-15 16:45:08 +0530

    add

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,45 +1,125 @@
-import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import img1 from "../../Assets/Images/image1.jpg"
-import img2 from "../../Assets/Images/image2.jpg"
-import img3 from "../../Assets/Images/image3.jpg"
-import img4 from "../../Assets/Images/image4.jpg"
-import img5 from "../../Assets/Images/image5.jpg"
-import img6 from "../../Assets/Images/image6.jpg"
-import img7 from "../../Assets/Images/image7.jpg"
-import "./Slider.css"
-const Slider = () => {
-  return (
-        <Carousel className="main-slide">
-                <div>
-                    <img src={img1} alt="pp" height="400px" />
+import {useEffect} from 'react';
+import './Slider.css';
+
+function Slider(){
+
+    let slideIndex = 1;
+
+    setInterval(()=>{
+        if(slideIndex===7){
+            slideIndex=1
+        }
+        slideIndex++;
+      showSlides(slideIndex);
+    },6000)
+
+    const handleCurrentSlide = (n)=>{
+        showSlides(slideIndex = n)
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("demo");
+        let captionText = document.getElementById("caption");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        captionText.innerHTML = dots[slideIndex-1].alt;
+    } 
+
+    useEffect(()=>{
+        showSlides(1);
+    },[])
+
+    return(
+        <div className="container" style={{height:"545px"}}>
+
+            {/* <!-- Full-width images with number text --> */}
+            <div className="mySlides">
+                <div style={{display:"flex"}}>
+                    <img src="https://www.kindmeal.my/photos/deal/5/590-3137-m.jpg" style={{width:"70%",height:"350px"}} alt="slide-1"/>
+                    <img src="https://www.kindmeal.my/photos/shop/4/467-3019-m.jpg" width="30%" alt="1"/>
                 </div>
-                <div>
-                    <img src={img2}  alt="pp" height="400px" />
+               
+            </div>
+
+            <div className="mySlides">
+                <div style={{display:"flex"}}>
+                    <img src="https://www.kindmeal.my/photos/deal/5/590-3137-m.jpg" style={{width:"70%",height:"350px"}} alt="slide-1"/>
+                    <img src="https://www.kindmeal.my/photos/shop/4/490-3254-m.jpg" width="30%" height="350px" alt="1"/>
                 </div>
-                <div>
-                    <img src={img3} alt="pp" height="400px"/>
-                   
+            </div>
+
+            <div className="mySlides">
+                <div style={{display:"flex"}}>
+                    <img src="https://www.kindmeal.my/photos/deal/6/633-3874-m.jpg" style={{width:"70%",height:"350px"}} alt="slide-1"/>
+                    <img src="https://www.kindmeal.my/photos/shop/5/512-3751-m.jpg" width="30%" height="350px" alt="1"/>
                 </div>
-                <div>
-                    <img src={img4} alt="pp" height="400px" />
-                   
+            </div>
+
+            <div className="mySlides">
+                <div style={{display:"flex"}}>
+                    <img src="https://www.kindmeal.my/photos/deal/6/618-3514-m.jpg" style={{width:"70%",height:"350px"}} alt="slide-1"/>
+                    <img src="https://www.kindmeal.my/photos/shop/4/494-3446-m.jpg" width="30%"  alt="1"/>
                 </div>
-                <div>
-                    <img src={img5} alt="pp" height="400px"/>
-                  
+            </div>
+
+            <div className="mySlides">
+                <div style={{display:"flex"}}>
+                    <img src="https://www.kindmeal.my/photos/deal/6/628-4921-m.jpg" style={{width:"70%",height:"350px"}} alt="slide-1"/>
+                    <img src="https://www.kindmeal.my/photos/shop/5/505-4609-m.jpg" width="30%" height="350px" alt="1"/>
                 </div>
-                <div>
-                    <img src={img6} alt="pp" height="400px"  />
-                   
+            </div>
+
+            <div className="mySlides">
+                <div style={{display:"flex"}}>
+                    <img src="https://www.kindmeal.my/photos/deal/4/498-2202-m.jpg" style={{width:"70%",height:"350px"}} alt="slide-1"/>
+                    <img src="https://www.kindmeal.my/photos/shop/2/212-2494-m.jpg" width="30%" height="350px" alt="1"/>
                 </div>
-                <div>
-                    <img src={img7}  alt="pp" height="400px" />
-                   
-                </div>
-            </Carousel>
-  )
+            </div>
+
+            {/* <!-- Image text --> */}
+            <div className="caption-container">
+                <p id="caption"></p>
+            </div>
+
+        {/* <!-- Thumbnail images --> */}
+        <div className="row" style={{paddingTop:"10px",paddingLeft:"10px"}}>
+            <div className="column">
+            <img className="demo cursor" src="https://www.kindmeal.my/photos/deal/6/683-4718-m.jpg" style={{width:"90%",height:"20vh"}} onClick={()=>handleCurrentSlide(1)} alt="Creative Home - Style Dinning,15%off -Red House 2"/>
+            </div>
+            <div className="column">
+            <img className="demo cursor" src="https://www.kindmeal.my/photos/deal/5/590-3137-m.jpg" style={{width:"90%",height:"20vh"}} onClick={()=>handleCurrentSlide(2)} alt="Memorial Vegetairian Delights, 20%off -Elym Vegetairian"/>
+            </div>
+            <div className="column">
+            <img className="demo cursor" src="https://www.kindmeal.my/photos/deal/6/633-3874-m.jpg" style={{width:"90%",height:"20vh"}} onClick={()=>handleCurrentSlide(3)} alt="Level Feals,20%off -Love Veggie"/>
+            </div>
+            <div className="column">
+            <img className="demo cursor" src="https://www.kindmeal.my/photos/deal/6/618-3514-m.jpg" style={{width:"90%",height:"20vh"}} onClick={()=>handleCurrentSlide(4)} alt="Trendy Fuision Delights"/>
+            </div>
+            <div className="column">
+            <img className="demo cursor" src="https://www.kindmeal.my/photos/deal/6/628-4921-m.jpg" style={{width:"90%",height:"20vh"}} onClick={()=>handleCurrentSlide(5)} alt="Japanese Fusion Delights"/>
+            </div>
+            <div className="column">
+            <img className="demo cursor" src="https://www.kindmeal.my/photos/deal/4/498-2202-m.jpg" style={{width:"90%",height:"20vh"}} onClick={()=>handleCurrentSlide(6)} alt="Imperial Rest,10% off -Implerial firstbColl"/>
+            </div>
+        </div>
+    </div>
+    )
 }
 
-export default Slider
+export default Slider;
+
+//  {
+//     img7:"https://www.kindmeal.my/photos/deal/6/617-3503-m.jpg",
+//     brand7:'https://www.kindmeal.my/photos/shop/4/493-3436-m.jpg',
+//     title:"Earth Friendly Meals,15off -LOovem "
+// }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,13 +4,20 @@ import Slider from '../Components/Slider/Slider'
 import Footer from '../Components/Footer/Footer'
 import HeroCards from '../Components/HeroCards/HeroCards'
 import Navbar from '../Components/Navbar/Navbar'
-
+import banner from '../Assets/Images/banner.png'
+import media from '../Assets/Images/mediafeature.png'
+import './HomePage.css'
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Slider/>
       <HeroCards />
+      <div className='white'>
+      <h1 className='featureON'>Feature On</h1>
+      <img src={media} alt="p" className='media'/>
+      <img src={banner} alt="p" className='banner'/>
+      </div>
       <Footer />
     </div>
   )
