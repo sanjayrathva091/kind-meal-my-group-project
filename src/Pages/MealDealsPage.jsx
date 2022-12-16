8bccc68b54ed2acd3f7ad6bd8492025a6d1bae29
Shani Sonkar <shanikkt1998@gmail.com>
2022-12-16 12:53:05 +0530

    day-3

---
+++ src/Pages/MealDealsPage.jsx
@@ -0,0 +1,285 @@
+import React from 'react';
+import styles from "./mealdeals.module.css";
+
+const MealDealsPage = () => {
+  return (
+    <div>
+        <div className={styles.container}>
+            <div className={styles.secondDiv}>
+                <div className={styles.thirdDiv}>
+                    <p className={styles.firstPara}>Meat-Free Deals <span>| Restaurants In Malaysia</span></p>
+                    <p className={styles.secondPara}>
+                        Browse delicious meat-free, vegetarian deals 
+                        from top restaurants and cafes! Just click on 
+                        "Get Free Coupon" to obtain instant discounts and 
+                        dine at the restaurants. No upfront payment, 
+                        booking or printing is needed. If you share 
+                        it on social media, 
+                        you'll even DOUBLE your discount!
+                    </p>
+                    <p>Download our <span>mobile app</span> now to easily get coupons and start dining in a few seconds. Effortlessly save lives, health, environment and money now!</p>
+                    <div className={styles.forthDiv}>
+                        <input type="text" placeholder='Search Shop or Deal Name'/>
+                        <select name="" id="">
+                            <option value="#">All Categories</option>
+                        </select>
+                        <select name="" id="">
+                            <option value="">All Locations</option>
+                        </select>
+                        <button>Search Deals</button>
+                        <button>Browse Restaurants</button>
+                    </div>
+                </div>   
+            </div>
+        </div>
+        <div className={styles.bannerDiv}>
+            <img width="385px" height="257px" src="https://www.kindmeal.my/photos/shop/4/493-3440-m.jpg" alt="" />
+            <img margin-right="10px" width="385px" height="257px" src="https://www.kindmeal.my/photos/deal/6/617-3503-m.jpg" alt="" />
+            <img width="250px" height="257px" src="https://www.kindmeal.my/photos/shop/4/493-3436-m.jpg" alt="" />
+        </div>
+        <div className={styles.gridDiv}>
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/7/713-5140-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/4/467-2796-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/7/713-5140-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/4/467-2796-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/7/713-5140-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/4/467-2796-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/7/713-5140-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/4/467-2796-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/7/713-5140-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img className={styles.gridImage} src="https://www.kindmeal.my/photos/deal/4/467-2796-l.jpg" alt="" />
+                <div>
+                    <p>Vega Art - <span> Johor Bahru, Johor</span></p>
+                    <p>Feast on our signature Korean & Chinese cuisine, as well as hotpot and Western meals! Our healthy food and soups are prepared wit..</p>
+                    <button>Get FREE Coupen</button>
+                </div>
+                <div>
+                    <div>
+                        <img src="https://www.kindmeal.my/images/icon_egg.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_milk_disabled.png" alt="" />
+                        <img src="https://www.kindmeal.my/images/icon_alcohol_disabled.png" alt="" />
+                    </div>
+                    <div>
+                        <p>KindMeal Discount</p>
+                        <p>10% Off</p>
+                    </div>
+                    <div>
+                        <p>Expires In</p>
+                        <p>6 Days</p>
+                    </div>
+                </div>
+            </div>
+            
+
+        </div>
+        <div className={styles.lastDiv}>
+            <img src="https://www.kindmeal.my/images/how_kindmeal_works.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default MealDealsPage;
