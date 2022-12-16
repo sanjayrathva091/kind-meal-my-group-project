937dbb39bb2d304baa3d3e74f06d551415dd0a07
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-16 22:21:21 +0530

    add

--- src/Components/HeroCards/HeroCards.jsx
+++ src/Components/HeroCards/HeroCards.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./HeroCards.css";
 import datahompage from "./homepagemapdata";
 // import { useNavigate } from "react-router-dom";
-
+import icon1 from "../../Assets/Images/icon1.png";
+import icon2 from "../../Assets/Images/icon2.png";
+import icon3 from "../../Assets/Images/icon3.png";
+import icon4 from "../../Assets/Images/icon4.png";
 const HeroCards = () => {
-  // const navs = useNavigate();
+  // const //navs = useNavigate();
   return (
     <>
     <div className="bnn">
-      <div className="lighblue_box">
+    <div className="lighblue_box">
         <div className="homepage_box1">
           <div className="homepage_box1_headline">
             <div className="homepage_box1_headline_left">
@@ -23,9 +26,9 @@ const HeroCards = () => {
           </div>
           <div
             className="homepage_box1_cont"
-            // onClick={() => {
-            //   navs("/VideoDisplaypage");
-            // }}
+            onClick={() => {
+              // //navs("/VideoDisplaypage");
+            }}
           >
             {datahompage.map(({ id, category, img, describe }) => {
               if (category === "Latest News & Videos") {
@@ -56,9 +59,9 @@ const HeroCards = () => {
           </div>
           <div
             className="homepage_box1_cont"
-            // onClick={() => {
-            //   navs("/Article");
-            // }}
+            onClick={() => {
+              //navs("/Article");
+            }}
           >
             {datahompage.map(({ id, category, img, describe }) => {
               if (category === "Yummylicious Moments") {
@@ -89,9 +92,9 @@ const HeroCards = () => {
           </div>
           <div
             className="homepage_box1_cont"
-            // onClick={() => {
-            //   navs("/Article");
-            // }}
+            onClick={() => {
+              //navs("/Article");
+            }}
           >
             {datahompage.map(({ id, category, img, describe }) => {
               if (category === "Discover Restaurants") {
@@ -122,9 +125,9 @@ const HeroCards = () => {
           </div>
           <div
             className="homepage_box1_cont"
-            // onClick={() => {
-            //   navs("/Article");
-            // }}
+            onClick={() => {
+              // //navs("/Article");
+            }}
           >
             {datahompage.map(({ id, category, img, describe }) => {
               if (category === "Amazing Superheroes") {
@@ -137,6 +140,65 @@ const HeroCards = () => {
               }
             })}
           </div>
+        </div>
+      </div>
+      <div className="padpad_pad"></div>
+      <div className="bottom_section">
+        <div className="bottom_section_divs newsides">
+          <h1>
+            Introducing Malaysia's Pioneering Meat-Free Lifestyle Platform
+          </h1>
+        </div>
+        <div className="bottom_section_divs">
+          <p>
+            Instant coupon & dining. No upfront coupon payment, booking or
+            printing.
+          </p>
+        </div>
+        <div className="bottom_section_divs" id="flexes">
+          <div className="bottom_section_divs_imgbox">
+            <img src={icon1} alt="" />
+            <h2>Get Great Deals</h2>
+            <p>
+              Show our FREE digital coupons to instantly enjoy exciting deals
+            </p>
+          </div>
+
+          <div className="bottom_section_divs_imgbox">
+            <img src={icon2} alt="" />
+            <h2>Share KindMoments</h2>
+            <p>Spread the joy by sharing your yummy dining moments</p>
+          </div>
+
+          <div className="bottom_section_divs_imgbox">
+            <img src={icon3} alt="" />
+            <h2>Discover Delicious Food</h2>
+            <p>Explore the latest exquisite offerings and creative menus</p>
+          </div>
+
+          <div className="bottom_section_divs_imgbox">
+            <img src={icon4} alt="" />
+            <h2>Meet Food Lovers</h2>
+            <p>Make new, compassionate friends and share great food tips</p>
+          </div>
+        </div>
+        <div className="bottom_section_divs">
+          <p>
+            Any restaurant or cafe can join KindMeal, vegetarian or not, as long
+            as the deals and menu featured are meat-free. Enjoy a great
+            meat-free dining experience. Easily save animal lives, health,
+            environment and money now!
+          </p>
+        </div>
+
+        <div id="btnredred">
+          <button id="redbutton_mainpage">
+            <a href="">
+              <h1>Join KindMeal Now</h1>
+              <br />
+              <p>Your tasty journey begins here</p>
+            </a>
+          </button>
         </div>
       </div>
       </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 import Slider from '../Components/Slider/Slider'
 import Footer from '../Components/Footer/Footer'
 import HeroCards from '../Components/HeroCards/HeroCards'
-import Navbar from '../Components/Navbar/Navbar'
 import banner from '../Assets/Images/banner.png'
 import media from '../Assets/Images/mediafeature.png'
 import './HomePage.css'
 const HomePage = () => {
   return (
     <div>
-      <Navbar />
       <Slider/>
       <HeroCards />
       <div className='white'>
