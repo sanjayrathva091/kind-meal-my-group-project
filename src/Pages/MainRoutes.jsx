c13dbbc35403861b9b96a88f3438ac473f62cf62
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-18 16:48:10 +0530

    add error.page

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import HelpPage from "./HelpPage";
+import Error from "./Error";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/help" element={<HelpPage />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 };
