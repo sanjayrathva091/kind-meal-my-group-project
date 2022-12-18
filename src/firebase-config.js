81070c8fd3011d9a9917a7a29c12304b1b3d212c
sanjayrathva091 <sanjayrathva091@gmail.com>
2022-12-18 19:17:49 +0530

    ADDED:

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA6se10YOqlEc7ckboOnr_mFGGa2elbqgY",
-    authDomain: "kindmealmy.firebaseapp.com",
-    projectId: "kindmealmy",
-    storageBucket: "kindmealmy.appspot.com",
-    messagingSenderId: "850658187028",
-    appId: "1:850658187028:web:e7675a1430dfde65f71401"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
